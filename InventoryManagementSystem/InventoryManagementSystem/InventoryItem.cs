bef9a807d5c22f5c1ae8232f44d9f10f0fd0e736
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-08-11 23:29:14 +0000

    Add immutable InventoryItem record with required fields

---
+++ InventoryManagementSystem/InventoryManagementSystem/InventoryItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    public record InventoryItem
+    {
+        #region fields
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public int Quantity { get; init; }
+        public DateTime DateAdded { get; init; }
+
+        #endregion fields
+
+        #region constructors
+
+        // Constructor to initialize the record
+        public InventoryItem(int id, string name, int quantity, DateTime dateAdded)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Quantity = quantity;
+            this.DateAdded = dateAdded;
+        }
+        #endregion constructors
+
+    }
+
+}
